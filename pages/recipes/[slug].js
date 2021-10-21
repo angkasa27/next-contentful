6260a21beab2ae9999816f2702e451bfa2a9113b
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2021-10-21 18:13:30 +0700

    Add skeleton

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false, //jika halaman dituju tidak ada dalam page akan diarahkan ke fallback
+    fallback: true, //jika halaman dituju tidak ada dalam page akan diarahkan ke fallback
   };
 };
 
@@ -39,6 +40,8 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ data }) {
+  if (!data) return <Skeleton />;
+
   const { featuredImage, title, cookingTime, ingredients, method } =
     data.fields;
 
