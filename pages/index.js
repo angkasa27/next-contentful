cbce631016babe83dda341a9b8932cadde03e739
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2021-10-21 17:52:37 +0700

    Add files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import Card from "../components/Card";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      data: res.items,
+    },
+  };
+}
+
+export default function Recipes({ data }) {
   return (
     <div className="recipe-list">
-      Recipe List
+      {data.map((v) => (
+        <Card key={v.sys.id} data={v} />
+      ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,94 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  const paths = res.items.map((v) => {
+    return {
+      params: { slug: v.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false, //jika halaman dituju tidak ada dalam page akan diarahkan ke fallback
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      data: items[0],
+    },
+  };
+}
+
+export default function RecipeDetails({ data }) {
+  const { featuredImage, title, cookingTime, ingredients, method } =
+    data.fields;
+
   return (
     <div>
-      Recipe Details
+      <div className="banner">
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2> {title}</h2>
+      </div>
+      <div className="info">
+        <p>Take about {cookingTime} mins to cook.</p>
+        <h3>Ingredients</h3>
+        {ingredients.map((v) => (
+          <span key={v}>{v}</span>
+        ))}
+        <div className="method">
+          <h3>Method</h3>
+          <div>{documentToReactComponents(method)}</div>
+        </div>
+
+        <style jsx>{`
+          h2,
+          h3 {
+            text-transform: uppercase;
+          }
+          .banner h2 {
+            margin: 0;
+            background: #fff;
+            display: inline-block;
+            padding: 20px;
+            position: relative;
+            top: -60px;
+            left: -10px;
+            transform: rotateZ(-1deg);
+            box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          }
+          .info p {
+            margin: 0;
+          }
+          .info span::after {
+            content: ", ";
+          }
+          .info span:last-child::after {
+            content: ".";
+          }
+        `}</style>
+      </div>
     </div>
-  )
+  );
 }
